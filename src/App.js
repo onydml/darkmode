aeb58b52fa1419c654fd1bdeed22b748d88f85e7
onydml <43037991+onydml@users.noreply.github.com>
2021-11-30 20:39:23 +0700

    change header's name

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Title from './components/Title';
 import {createContext, useState} from "react"
 
 export const ThemeContext = createContext()
+
 function App() {
   // eslint-disable-next-line
   const [theme,setTheme] = useState("light")
