9e132cb6968586d38d080dc5a9e6f885914fbc25
onydml <43037991+onydml@users.noreply.github.com>
2021-11-30 20:15:13 +0700

    a

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import Title from './components/Title';
 import {createContext, useState} from "react"
 
 export const ThemeContext = createContext()
-
 function App() {
+  // eslint-disable-next-line
   const [theme,setTheme] = useState("light")
   return (
       <ThemeContext.Provider value={{theme,setTheme}}>
